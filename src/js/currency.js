bf8c72df17f1fcf8155f916dbd8163e6e9c91d3e
Min Chang <minchangmhc@gmail.com>
2021-04-23 12:01:29 -0700

    BUGFIX: .env not working.

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,6 +1,6 @@
 export default class CurrencyExchange {
   static async getExchange(currency) {
-    const response = await fetch (`https://v6.exchangerate-api.com/v6/230c8ba70e320bf625cb22e9/pair/USD/${currency}`);
+    const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`);
     return await response.json();
   }
 }
