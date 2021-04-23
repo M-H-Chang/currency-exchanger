6de1b69d3ca9d9aedb2caee0aeb2f8b3311f0168
Min Chang <minchangmhc@gmail.com>
2021-04-23 10:54:15 -0700

    WIP: displaying conversion to website.

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,6 +1,9 @@
 export default class CurrencyExchange {
   static async getExchange(currency) {
-    const response = await fetch (` https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currency}`);
+    const response = await fetch (`https://v6.exchangerate-api.com/v6/230c8ba70e320bf625cb22e9/pair/USD/${currency}`);
     return await response.json();
   }
 }
+// let html = `<p>`;
+//     html += `${currencyValue.result}: Currency not Supported`;
+//     $('#conversion').html(html);
--- src/main.js
+++ src/main.js
@@ -2,4 +2,23 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-console.log($);
+import CurrencyExchange from './js/currency.js';
+
+function showCurrency(currencyValue) {
+  let html = `<p>`;
+  html += `Converted: ${currencyValue.conversion_rate}`;
+  html += '</p>';
+  $('#conversion').html(html);
+}
+
+$('#form').submit(async function(event) {
+  event.preventDefault();
+  let curr = $('#currencyType option:selected').val();
+  try {
+    const currencyValue = await CurrencyExchange.getExchange(curr);
+    showCurrency(currencyValue);
+    console.log(currencyValue);
+  } catch (error) {
+    console.log(error);
+  }
+});
